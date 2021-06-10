26671cea642bb02597c88ad70543842b9748a524
Michael Stonis <michael.stonis@eightbot.com>
2021-06-09 22:58:33 -0500

    add app shell base

---
+++ eShopOnContainers/eShopOnContainers.Core/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell ()
+        {
+            InitializeRouting ();
+            InitializeComponent ();
+        }
+
+        private void InitializeRouting()
+        {
+            Routing.RegisterRoute (Views.MainView.RouteName, typeof (Views.MainView));
+        }
+    }
+}
